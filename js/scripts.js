d54a7a711fb285cbf104326ad9545c4007b91ce4
Alex Martinez <zanderalex377@gmail.com>
2019-04-05 15:03:51 -0700

    delete test folder

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 // Business logic
+var allNumbers = [0,1,2,3,4,5,6,7,8,9];
+var mostNumbers = [0,4,5,6,7,8,9];
+var specialOne = "1"
+var specialTwo = "2"
+var specialThree = "3"
 var numberInput = function(number) {
   if ((number === 3)
   return ("I'm sorry, Dave. I'm afraid I can't do that."); {

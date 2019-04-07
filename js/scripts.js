cc8b327e9c27e37f41470842149033ae12f5727b
Alex Martinez <zanderalex377@gmail.com>
2019-04-07 14:47:16 -0700

    create div ids for results, add readme template

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@
 //var allNumbers = [0,1,2,3,4,5,6,7,8,9];
 //create loop which runs until it reaches the number inputted
 //add all those numbers to input number and create string
-//all numbers including 1 = BEEP
-//unless number includes 2 = BOOP
-//unless number includes 3 = I'm sorry...
+//display inputted number "resultZero"
+//unless number includes a 1 = BEEP  "resultOne"
+//unless number includes a 2 = BOOP  "resultTwo"
+//unless number includes a 3 = I'm sorry...  "resultThree"
+
 var mostNumbers = [0,4,5,6,7,8,9];
 
 var isNumber = function(userInputArray){

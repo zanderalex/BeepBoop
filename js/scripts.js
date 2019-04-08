1617b3586f61ab0613572d55eb7e1d3a8bcbc2be
Alex Martinez <zanderalex377@gmail.com>
2019-04-08 02:50:27 -0700

    reconcile inconsistancies between index and scripts

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,9 @@ $(document).ready(function() {
     writeToAnswerAreaBasedOn(userInput);
   });
 });
+
+
+
 // Business logic
 //create loop which runs until it reaches the number inputted
 //add all those numbers to input number and create string
@@ -13,7 +16,6 @@ $(document).ready(function() {
 //unless number includes a 1 = BEEP  "resultOne"
 //unless number includes a 2 = BOOP  "resultTwo"
 //unless number includes a 3 = I'm sorry...  "resultThree"
-
 
 var getUserInput = function() {
   return $("#userInput").val().toString();
@@ -44,4 +46,24 @@ var generateHtmlBasedOn = function(numString) {
   }
 
   return listOfHtmlElements;
+};
+
+var getBeepBoopSorryOrNumFrom = function(numString) {
+  var currNum = "" + numString;
+
+  // deal with any 3 in the numString
+  if (currNum.indexOf("3") != -1) {
+    return "I'm sorry Dave, I can't do that.";
+  }
+
+  // deal with any 2
+  if (currNum.indexOf("2") != -1) {
+    return "Boop!";
+  }
+  // deal with any 1
+  if (currNum.indexOf("1") != -1) {
+    return "Beep!";
+  }
+  // otherwise just
+  return numString;
 };

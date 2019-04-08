1b253509477a7d32e2cbb294110a628d13385df7
Alex Martinez <zanderalex377@gmail.com>
2019-04-08 02:03:23 -0700

    add new bootstrap path

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
+//User Interface Logic
+$(document).ready(function() {
+  // get user input
+  $("#submitButton").click(function() {
+    var userInput = getUserInput();
+    writeToAnswerAreaBasedOn(userInput);
+  });
+});
 // Business logic
-//var allNumbers = [0,1,2,3,4,5,6,7,8,9];
 //create loop which runs until it reaches the number inputted
 //add all those numbers to input number and create string
 //display inputted number "resultZero"
@@ -7,29 +14,34 @@
 //unless number includes a 2 = BOOP  "resultTwo"
 //unless number includes a 3 = I'm sorry...  "resultThree"
 
-var mostNumbers = [0,4,5,6,7,8,9];
 
-var isNumber = function(userInput){
-  if (userInput >=0 || userInput <=0) {
-    return false;
+var getUserInput = function() {
+  return $("#userInput").val().toString();
+};
+
+var writeToAnswerAreaBasedOn = function(input) {
+  // write results to answer area
+  $("#answerArea").html(generateHtmlBasedOn(input));
+};
+
+var generateHtmlBasedOn = function(numString) {
+  // deal with the case where the user didn't enter anything
+  if (numString == "") {
+    // return an error message with instructions
+    return [
+      "<h1>I seem to have received nothing!</h1>",
+      "<p>Please enter a number</p>"
+    ];
   }
 
-var input = function(number) {
-  if (number.includes(3)
-  return #resultOne; {
-  } else if (input.includes(2) console.log("Boop!"); {
-  } else if (input.includes(1) console.log("Beep!"); {
-  });
-});
+  var listOfHtmlElements = [];
 
+  for (var currNum = 0; currNum <= numString; currNum++) {
+    var htmlElt = "<p class=\"bbLine\">"
+      .concat(getBeepBoopSorryOrNumFrom(currNum))
+      .concat("</p>");
+    listOfHtmlElements.push(htmlElt);
+  }
 
-
-//User Interface Logic
-$(document).ready(function() {
-  $("form#beep-boop").submit(function(event) {
-    event.preventDefault();
-    var number = parseInt($("input#number").val());
-    var result = input(number);
-    $("#result").text(result);
-  });
-});
+  return listOfHtmlElements;
+};
